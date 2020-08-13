1c51f8cb3ef59a238921912fae5c2206315a785b
ScholasticTolase <nureni.tolase@gmail.com>
2020-08-13 01:17:38 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gearhead.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            dynamic Home = ViewBag.Title;
+            return View();
+        }
+
+        public ActionResult Engine()
+        {
+            dynamic Engine = ViewBag.Title;
+            return View();
+        }
+
+        public ActionResult Fuelsystem()
+        {
+            return View();
+        }
+
+        public ActionResult Safety()
+        {
+            return View();
+        }
+
+        public ActionResult Ignitionsystem()
+        {
+            return View();
+        }
+
+        public ActionResult Drivetrain()
+        {
+            return View();
+        }
+        public ActionResult Suspension()
+        {
+            return View();
+        }
+
+        public ActionResult Brakesystem()
+        {
+            return View();
+        }
+    }
+
+
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gearhead.MvcApplication" Language="C#" %>
